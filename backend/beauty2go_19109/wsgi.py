25ac7c531b3a87ac23935dfe7e7bd682da2c5754
Crowdbotics <team@crowdbotics.com>
2020-07-22 20:30:42 +0000

    Initial commit.

---
+++ backend/beauty2go_19109/wsgi.py
@@ -0,0 +1,16 @@
+"""
+WSGI config for beauty2go_19109 project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/2.2/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'beauty2go_19109.settings')
+
+application = get_wsgi_application()
